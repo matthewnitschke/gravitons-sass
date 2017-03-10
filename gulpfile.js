358cfe68ba2f9988f8668364aeb6b5bc51de7d51
Matthew <matthewcnitschke@gmail.com>
2017-03-10 15:45:21 -0700

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var rename = require("gulp-rename");
+var cssmin = require('gulp-cssmin');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  gulp.src("src/gravitons.scss")
+    .pipe(sass())
+    .pipe(rename("gravitons.css"))
+    .pipe(gulp.dest('dist'));
+
+    gulp.src("src/gravitons.scss")
+      .pipe(sass())
+      .pipe(cssmin())
+      .pipe(rename("gravitons.min.css"))
+      .pipe(gulp.dest('dist'));
+})
